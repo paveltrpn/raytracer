cdd49b74647284ddd09c1c089e754b60cbc03b42
Pavel Turapin <paveltrpn@integra-s.com>
2025-08-12 10:51:10 +0400

    add glfw window

--- tire/src/main/kotlin/tire/Main.kt
+++ tire/src/main/kotlin/tire/Main.kt
@@ -4,4 +4,10 @@ import window.Window
 
 fun main(args: Array<String>) {
     println("tire")
+
+    val window: Window = Window()
+
+    window.loop()
+
+    window.destroy()
 }
--- tire/src/main/kotlin/window/Window.kt
+++ tire/src/main/kotlin/window/Window.kt
@@ -1,12 +1,14 @@
 package window
 
 import org.lwjgl.glfw.*
-import org.lwjgl.glfw.*;
 import org.lwjgl.glfw.GLFW.*
-import org.lwjgl.opengl.*;
-import org.lwjgl.system.*;
+import org.lwjgl.opengl.*
+import org.lwjgl.system.*
 import org.lwjgl.system.MemoryStack.*
 import org.lwjgl.system.MemoryUtil.*
+import java.util.*
+import java.util.function.IntFunction
+
 
 class Window {
     private var allocator: GLFWAllocator? = null
@@ -40,11 +42,68 @@ class Window {
         val WIDTH = 300
         val HEIGHT = 300
 
-        val window: Long = glfwCreateWindow(WIDTH, HEIGHT, "GLFW Gears Demo", NULL, NULL)
+        window = glfwCreateWindow(WIDTH, HEIGHT, "GLFW Gears Demo", NULL, NULL)
         if (window == NULL) {
             throw RuntimeException("Failed to create the GLFW window")
         }
 
+        glfwSetWindowSizeLimits(window, WIDTH, HEIGHT, GLFW_DONT_CARE, GLFW_DONT_CARE)
+
+        //glfwSetWindowAspectRatio(window, 1, 1);
+        val monitor = glfwGetPrimaryMonitor()
+
+        val vidmode: GLFWVidMode? = glfwGetVideoMode(monitor)
+
+        glfwSetWindowPos(
+            window,
+            (vidmode!!.width() - WIDTH) / 2,
+            (vidmode.height() - HEIGHT) / 2
+        )
+
+//        glfwSetFramebufferSizeCallback(window, this::framebufferSizeChanged)
+
+//        glfwSetWindowRefreshCallback(window, { windowHnd ->
+//            gears.render()
+//            gears.animate()
+//            glfwSwapBuffers(windowHnd)
+//        })
+
+        glfwMakeContextCurrent(window)
+        GL.createCapabilities(IntFunction { num: Int -> memCallocPointer(num) })
+//        debugProc = GLUtil.setupDebugMessageCallback()
+
+
+        glfwSwapInterval(1)
+        glfwShowWindow(window)
+
+//        glfwInvoke(window, null, this::framebufferSizeChanged)
     }
 
+    fun loop() {
+        var lastUpdate = System.currentTimeMillis()
+
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents()
+
+            glfwSwapBuffers(window)
+        }
+    }
+
+    fun destroy() {
+        memFree(GL.getCapabilities().getAddressBuffer())
+        GL.setCapabilities(null)
+
+//        if (window !== NULL) {
+//            glfwFreeCallbacks(window)
+//            glfwDestroyWindow(window)
+//        }
+
+        glfwTerminate()
+//        Objects.requireNonNull<T?>(glfwSetErrorCallback(null)).free()
+
+        allocator!!.deallocate().free()
+        allocator!!.reallocate().free()
+        allocator!!.allocate().free()
+        allocator!!.free()
+    }
 }
