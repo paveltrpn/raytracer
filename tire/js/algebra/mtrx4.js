e92d7ce88263e54fb12403383f83d662cdeae8b5
Pavel Turapin <paveltrpn@integra-s.com>
2025-09-12 16:36:14 +0400

    fix lint warnings

--- tire/js/algebra/mtrx4.js
+++ tire/js/algebra/mtrx4.js
@@ -290,29 +290,33 @@ export class mtrx4 {
 	}
 
 	setAxisAngl(axis, phi) {
-		let cosphi, sinphi, vxvy, vxvz, vyvz, vx, vy, vz;
 		let ax = new vec3.vec3();
 		ax = vec3.vec3Normalize(axis);
-		cosphi = Math.cos(phi);
-		sinphi = Math.sin(phi);
-		vxvy = ax.data[0] * ax.data[1];
-		vxvz = ax.data[0] * ax.data[2];
-		vyvz = ax.data[1] * ax.data[2];
-		vx = ax.data[0];
-		vy = ax.data[1];
-		vz = ax.data[2];
+
+		const cosphi = Math.cos(phi);
+		const sinphi = Math.sin(phi);
+		const vxvy = ax.data[0] * ax.data[1];
+		const vxvz = ax.data[0] * ax.data[2];
+		const vyvz = ax.data[1] * ax.data[2];
+		const vx = ax.data[0];
+		const vy = ax.data[1];
+		const vz = ax.data[2];
+
 		this.data[0] = cosphi + (1.0 - cosphi) * vx * vx;
 		this.data[1] = (1.0 - cosphi) * vxvy - sinphi * vz;
 		this.data[2] = (1.0 - cosphi) * vxvz + sinphi * vy;
 		this.data[3] = 0.0;
+
 		this.data[4] = (1.0 - cosphi) * vxvy + sinphi * vz;
 		this.data[5] = cosphi + (1.0 - cosphi) * vy * vy;
 		this.data[6] = (1.0 - cosphi) * vyvz - sinphi * vx;
 		this.data[7] = 0.0;
+
 		this.data[8] = (1.0 - cosphi) * vxvz - sinphi * vy;
 		this.data[9] = (1.0 - cosphi) * vyvz + sinphi * vx;
 		this.data[10] = cosphi + (1.0 - cosphi) * vz * vz;
 		this.data[11] = 0.0;
+
 		this.data[12] = 0.0;
 		this.data[13] = 0.0;
 		this.data[14] = 0.0;
@@ -320,25 +324,28 @@ export class mtrx4 {
 	}
 
 	setEuler(yaw, pitch, roll) {
-		let cosy, siny, cosp, sinp, cosr, sinr;
-		cosy = Math.cos(yaw);
-		siny = Math.sin(yaw);
-		cosp = Math.cos(pitch);
-		sinp = Math.sin(pitch);
-		cosr = Math.cos(roll);
-		sinr = Math.sin(roll);
+		const cosy = Math.cos(yaw);
+		const siny = Math.sin(yaw);
+		const cosp = Math.cos(pitch);
+		const sinp = Math.sin(pitch);
+		const cosr = Math.cos(roll);
+		const sinr = Math.sin(roll);
+
 		this.data[0] = cosy * cosr - siny * cosp * sinr;
 		this.data[1] = -cosy * sinr - siny * cosp * cosr;
 		this.data[2] = siny * sinp;
 		this.data[3] = 0.0;
+
 		this.data[4] = siny * cosr + cosy * cosp * sinr;
 		this.data[5] = -siny * sinr + cosy * cosp * cosr;
 		this.data[6] = -cosy * sinp;
 		this.data[7] = 0.0;
+
 		this.data[8] = sinp * sinr;
 		this.data[9] = sinp * cosr;
 		this.data[10] = cosp;
 		this.data[11] = 0.0;
+
 		this.data[12] = 0.0;
 		this.data[13] = 0.0;
 		this.data[14] = 0.0;
