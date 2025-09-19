3c4f4ab5e7f79374da82edae282b9ce12bd6ceff
Pavel Turapin <paveltrpn@integra-s.com>
2025-09-19 15:43:05 +0400

    move server to typescript

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,14 @@
-import express from "express";
+import express, { RequestHandler, Response } from "express";
 import path from "node:path";
 
 const app = express();
-const port = 8081;
+const port: number = 8081;
 
 const workpath = process.cwd();
 
 console.log(`Workpath is ${workpath}`);
 
-app.get("/", (req, res) => {
+app.get("/", (req: RequestHandler, res: any) => {
     res.sendFile(path.join(workpath, "tire", "index.html"));
 });
 
