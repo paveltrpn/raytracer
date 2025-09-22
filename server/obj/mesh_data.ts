8f238297c2ffd3c6c2c3154173cf31a525250822
Pavel Turapin <paveltrpn@integra-s.com>
2025-09-22 09:55:00 +0400

    update

--- server/obj/mesh_data.ts
+++ server/obj/mesh_data.ts
@@ -2,28 +2,55 @@
  * @brief Contains mesh data for exactly one object.
  */
 export class MeshData {
+    /**
+     * Array of vertecies, tri-component vectors.
+     * [{float: x, float: y, float z}, {...}, {...}, ...]
+     */
+    vertecies: Float32Array;
+
+    /**
+     * Array of triangles, contains indecies to vertecies array.
+     */
+    indicies: Uint32Array;
+
+    /**
+     * Two component vectors.
+     * [{float: u, float: v}, {...}, {...}, ...]
+     */
+    texcrds: Float32Array;
+
+    /**
+     * Per vertex normals.
+     */
+    normals: Float32Array;
+
+    /**
+     * Per vertex colors.
+     */
+    vertclrs: Float32Array;
+
     constructor() {
         //
     }
 
     setVertecies(vertecies: Float32Array) {
-        //
+        this.vertecies = vertecies;
     }
 
-    setIndicies(indicies: Int32Array) {
-        //
+    setIndicies(indicies: Uint32Array) {
+        this.indicies = indicies;
     }
 
     setTexCoords(tc: Float32Array) {
-        //
+        this.texcrds = this.texcrds;
     }
 
     setNormals(normals: Float32Array) {
-        //
+        this.normals = normals;
     }
 
-    setVertexColors(vs: Float32Array) {
-        //
+    setVertexColors(vc: Float32Array) {
+        this.vertclrs = vc;
     }
 
     toJson() {
--- server/obj/obj.ts
+++ server/obj/obj.ts
@@ -1,5 +1,9 @@
 import { MeshData } from "./mesh_data.ts";
 
+/**
+ * @brief - read wavefornt obj file in object, contains
+ * all mesh related data.
+ */
 export function readObj(path: string): MeshData {
     return new MeshData();
 }
