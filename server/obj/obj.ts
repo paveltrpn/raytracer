c050ebb4cbda0874674671ccb1c1fafe118ea486
Pavel Turapin <paveltrpn@integra-s.com>
2025-09-22 10:59:19 +0400

    extend doc

--- server/obj/obj.ts
+++ server/obj/obj.ts
@@ -5,10 +5,11 @@ import path from "node:path";
 /**
  * @brief - read wavefornt obj file in object, contains
  * all mesh related data.
+ * @throws {Error} - when file not exist.
  */
 export function readObj(filePath: string): MeshData {
     if (!fs.existsSync(filePath)) {
-        throw Error(`File ${filePath} not exist`);
+        throw new Error(`File ${filePath} not exist`);
     }
 
     // Get filename without extension.
